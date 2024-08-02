fe1608662f2ff818b3f9fede51fe7ded7ee97bf0
Abdul Karim Kevin DIAKITE <diakiteabdulkarim094@gmail.com>
2024-08-02 17:10:21 +0000

    add Dockerfile and configure github actions

--- todo_app/models.py
+++ todo_app/models.py
@@ -5,6 +5,7 @@ from django.db import models
 # Create your models here.
 
 class Todo(models.Model):
+
     title = models.CharField(max_length=200)
     description = models.TextField()
     due_date = models.DateField()
